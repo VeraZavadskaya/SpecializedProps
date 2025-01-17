fdd30cb11c3de77f1b7d45285af3033c22821aad
Вера Завадская <vera_zavadskaya_05@mail.ru>
2025-01-17 15:45:15 +0300

    utufghgv

--- SpecializedProps/View/Windows/AddCustomerWindow.xaml.cs
+++ SpecializedProps/View/Windows/AddCustomerWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SpecializedProps.AppData;
+using SpecializedProps.Model;
 
 namespace SpecializedProps.View.Windows
 {
@@ -22,6 +24,33 @@ namespace SpecializedProps.View.Windows
         public AddCustomerWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddCustomerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(LastNameTb.Text)&& string.IsNullOrEmpty(FirstNameTb.Text) && string.IsNullOrEmpty(MiddleNameTb.Text) && string.IsNullOrEmpty(NumberPhoneTb.Text) && string.IsNullOrEmpty(EmailTb.Text) && string.IsNullOrEmpty(AdressTb.Text) && string.IsNullOrEmpty(PasportTb.Text))
+            {
+                MessageBoxHelper.Warning("Заполните все поля!");
+            }
+            else
+            {
+                Customer newCustomer = new Customer()
+                {
+                    LastName = LastNameTb.Text,
+                    FirstName = FirstNameTb.Text,
+                    MiddleName = MiddleNameTb.Text,
+                    NumberPhone = NumberPhoneTb.Text,
+                    Email = EmailTb.Text,
+                    Adress = AdressTb.Text,
+                    Pasport = PasportTb.Text
+                };
+
+                App.context.Customer.Add(newCustomer);
+                App.context.SaveChanges();
+                MessageBoxHelper.Information("Заказчик добавлен!");
+
+
+            }
         }
     }
 }
--- SpecializedProps/View/Windows/OrderWindow.xaml.cs
+++ SpecializedProps/View/Windows/OrderWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using SpecializedProps.AppData;
 using SpecializedProps.Model;
 
 namespace SpecializedProps.View.Windows
@@ -45,7 +47,24 @@ namespace SpecializedProps.View.Windows
 
         private void AddOrderBtn_Click(object sender, RoutedEventArgs e)
         {
+            if(string.IsNullOrEmpty(NameCustomerCmb.Text) && string.IsNullOrEmpty(MaterialCmb.Text) && string.IsNullOrEmpty(ImagePathTbl.Text) && string.IsNullOrEmpty(ProductionDateTb.Text) && string.IsNullOrEmpty(EndDateTb.Text) && string.IsNullOrEmpty(DirectionTb.Text) && string.IsNullOrEmpty(CommentTb.Text) || string.IsNullOrEmpty(AdressDeliveryTb.Text) || string.IsNullOrEmpty(PickUpCmb.Text) && string.IsNullOrEmpty(PaymentCmb.Text) && string.IsNullOrEmpty(SummTb.Text))
+            {
+                MessageBoxHelper.Warning("Заполните все поля!");
+            }
+            else
+            {
+                Order newOrder = new Order()
+                {
+                    Customer = NameCustomerCmb.SelectedItem as Customer,
+                    FurnitureMaterial = MaterialCmb.SelectedItem as FurnitureMaterial,
+                    PhotoSketch = ImagePathTbl.Text,
+                    DateStart = (DateTime)ProductionDateTb.Text,
+                    DateFinish = (DateTime)EndDateTb.Text,
+                    Description = DirectionTb.Text,
+                    Comment = CommentTb.Text,
 
+                };
+            }
         }
 
         private void MaterialCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -73,20 +92,32 @@ namespace SpecializedProps.View.Windows
             }
         }
 
-        private void DeliveryCb_Checked(object sender, RoutedEventArgs e)
+        private void AddCustomerBtn_Click(object sender, RoutedEventArgs e)
         {
-            AdressDeliveryTb.IsEnabled = true;
+            AddCustomerWindow addCustomerWindow = new AddCustomerWindow();
+            addCustomerWindow.Hide();
+
         }
 
-        private void DeliveryCb_Unchecked(object sender, RoutedEventArgs e)
+        private void RadioButton_Unchecked(object sender, RoutedEventArgs e)
         {
             AdressDeliveryTb.IsEnabled = false;
             AdressDeliveryTb.Clear();
         }
 
-        private void AddCustomerBtn_Click(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            AdressDeliveryTb.IsEnabled = true;
+        }
 
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            PickUpCmb.IsEnabled = true;
+        }
+
+        private void RadioButton_Unchecked_1(object sender, RoutedEventArgs e)
+        {
+            PickUpCmb.IsEnabled = false;
         }
     }
 }
